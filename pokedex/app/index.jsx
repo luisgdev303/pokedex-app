7bb7f5ebc2bf6e8f6964f34bfbeddfe9da4c3f01
luisgdev303 <luisg.dev.lu@gmail.com>
2026-06-10 14:21:05 -0300

    Paramos na página 44.

---
+++ pokedex/app/index.jsx
@@ -0,0 +1,39 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { pokemons } from "@/data/pokemons";
+import Header from "@/components/Header";
+import Card from "../components/Card";
+
+export default function Index() {
+
+  const renderPokemon = ({ item }) => (
+    <Card pokemon={item} />
+  );
+
+  return (
+    <View style={styles.container}>
+      <Header title="Pokédex" />
+
+      <FlatList
+        data={pokemons}
+        style={styles.cards}
+        keyExtractor={(pokemon) => pokemon.Numero.toString()}
+        renderItem={renderPokemon}
+        initialNumToRender={10}
+        maxToRenderPerBatch={10}
+        windowSize={5}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    flexDirection: "column",
+  },
+
+  cards: {
+    padding: 15,
+  },
+});
